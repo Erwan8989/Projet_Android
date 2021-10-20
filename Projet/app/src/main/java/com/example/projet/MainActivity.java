0734fa661befd3453ce34a35c124b7db17d10bbf
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-10-20 17:11:47 +0200

    ...

--- Projet/app/src/main/java/com/example/projet/MainActivity.java
+++ Projet/app/src/main/java/com/example/projet/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.projet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1, b2;
+    Button b1, b2, b10;
+
+    Input input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-    input = findViewById(R.id.text);
-    textview = (TextView) findViewById(R.id.text);
+      input = findViewById(R.id.text);
+      textview = (TextView) findViewById(R.id.text);
 
     // ***************** Obtenir l'API, et l'afficher *****************
 
