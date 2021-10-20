0442b333def2e2ade5fd9a62cb717dea8ebe0db3
alexwtrs <alexandre.wauters@epsi.fr>
2021-10-20 12:08:05 +0200

    Push for Erwan

--- Projet/app/src/main/java/com/example/projet/MainActivity.java
+++ Projet/app/src/main/java/com/example/projet/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.projet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url ="https://api.github.com/zen";
-    Button b10;
+    String url = "https://api.github.com/zen";
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.connexion_inscription);
+        b1 = (Button) findViewById(R.id.button1);
 
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
 
-
+    public void openActivity2() { // Fonction qui permet d'aller à l'activité 2
+        Intent intent = new Intent(this, connexion_inscription.class);
+        startActivity(intent);
+    }
 }
