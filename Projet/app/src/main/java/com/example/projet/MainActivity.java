93c826588d9037efd3b8b704f16e5f4e0ede5836
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-10-20 18:00:00 +0200

    Update MainActivity.java

--- Projet/app/src/main/java/com/example/projet/MainActivity.java
+++ Projet/app/src/main/java/com/example/projet/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1, b2;
+    Button b1, b2, b10;
+  String url ="http://localhost/projet_android/conn_bdd.php?pseudo=tata&mail=oiuaz&mdp=toto";
+  TextView textview;
+  EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
