776d53e731cd36d1366f847efb350d530f77633d
ArthurBreEpsi <arthur.bre@epsi.fr>
2021-11-17 12:41:24 +0100

    update java

--- Projet/app/src/main/java/com/example/projet/conversations_liste.java
+++ Projet/app/src/main/java/com/example/projet/conversations_liste.java
@@ -1,4 +1,20 @@
 package com.example.projet;
 
-public class conversations_liste {
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projet.R;
+
+public class conversations_liste extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conversations_liste);
+    };
 }
