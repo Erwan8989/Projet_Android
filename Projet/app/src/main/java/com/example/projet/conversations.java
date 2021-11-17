5b2a47c7e6fbf096045fc90c82a00c9cc28b5199
ArthurBreEpsi <arthur.bre@epsi.fr>
2021-11-17 12:24:19 +0100

    lien page parametres

--- Projet/app/src/main/java/com/example/projet/conversations.java
+++ Projet/app/src/main/java/com/example/projet/conversations.java
@@ -1,18 +1,50 @@
 package com.example.projet;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class conversations extends AppCompatActivity {
 
+    Button b4, b5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conversations);
 
+        b4 = (Button) findViewById(R.id.button4);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity4();
+            }
+        });
+
+        b5 = (Button) findViewById(R.id.button5);
+            b5.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openActivity5();
+                }
+            });
+
     }
 
+    public void openActivity4() {
+        Intent parametres = new Intent(this, parametres.class);
+        startActivity(parametres);
+        finish();
+    }
+
+    public void openActivity5() {
+        Intent parametres_informations_personnelles = new Intent(this, parametres_informations_personnelles.class);
+        startActivity(parametres_informations_personnelles);
+        finish();
+    }
 }
