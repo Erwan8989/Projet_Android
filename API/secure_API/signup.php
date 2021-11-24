93741ac2bf284f18b60aa33fd3e3735b3a347dbd
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-11-24 15:33:10 +0100

    Revert "Revert "API (en cours)""
    This reverts commit 00ed98015613f7685680ef42f135d1cd27cae820.

--- API/secure_API/signup.php
+++ API/secure_API/signup.php
@@ -4,8 +4,8 @@ $db = new DataBase();
 if (isset($_POST['email']) && isset($_POST['pseudo']) && isset($_POST['mdp'])) {
     if ($db->dbConnect()) {
         if ($db->signUp("users", $_POST['email'], $_POST['pseudo'], $_POST['mdp'])) {
-            echo "Sign Up Success";
-        } else echo "Sign up Failed";
-    } else echo "Error: Database connection";
-} else echo "All fields are required";
+            echo "Inscription  réussite";
+        } else echo "Erreur de connexion àà la base donnéee";
+    } else echo "Erreur: Inscription  à la base de donnée";
+} else echo "Tout les champs sont requis";
 ?>
