00ed98015613f7685680ef42f135d1cd27cae820
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-11-24 15:23:55 +0100

    Revert "API (en cours)"
    This reverts commit b17adb23f08238e70e41ab3ecabfe24bd2d4bd83.

--- API/secure_API/DataBase.php
+++ API/secure_API/DataBase.php
@@ -20,9 +20,6 @@ class DataBase
         $this->servername = $dbc->servername;
         $this->username = $dbc->username;
         $this->password = $dbc->password;
-        // $this->messages = $dbc->messages;
-        // $this->date = $dbc->date;
-
         $this->databasename = $dbc->databasename;
     }
 
@@ -66,17 +63,6 @@ class DataBase
         if (mysqli_query($this->connect, $this->sql)) {
             return true;
         } else return false;
-    }
-
-    function getData ($table, $messages, $date)
-    {
-        $messages = $this->prepareData($messages);
-        $dates = $this->prepareData($dates);
-        $this->sql = "SELECT * FROM . $table . WHERE id = 1";
-        $result = mysqli_query($this->connect, $this->sql);
-        $row = mysqli_fetch_assoc($result);
-        // $result = $conn->query($sql);
-
     }
 
 }
